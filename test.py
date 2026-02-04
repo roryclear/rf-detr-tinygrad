2ba48c6b140cf10ce00bc521e7f7847c2b6881cb
roryclear <roryclear.rc@gmail.com>
2026-02-04 14:56:03 +0000

    PositionEmbeddingSine forward in tg

--- test.py
+++ test.py
@@ -1541,25 +1541,21 @@ class PositionEmbeddingSine(nn.Module):
 
     def forward(self, tensor_list: NestedTensor, align_dim_orders = True):
         mask = tensor_list.mask
-
         if type(mask) != tinyTensor: mask = to_tiny(mask)
         not_mask = ~mask
-        not_mask = to_torch(not_mask)
         y_embed = not_mask.cumsum(1)
         x_embed = not_mask.cumsum(2)
         eps = 1e-6
         y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
         x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
-
-        dim_t = torch.arange(self.num_pos_feats)
+        dim_t = tinyTensor.arange(self.num_pos_feats)
         dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
-
         pos_x = x_embed[:, :, :, None] / dim_t
         pos_y = y_embed[:, :, :, None] / dim_t
-        pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
-        pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
-        pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-        return pos
+        pos_x = tinyTensor.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
+        pos_y = tinyTensor.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
+        pos = tinyTensor.cat(pos_y, pos_x, dim=3).permute(0, 3, 1, 2)
+        return to_torch(pos)
 
 class Backbone(BackboneBase):
     """backbone."""
