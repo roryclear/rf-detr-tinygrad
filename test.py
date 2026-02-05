5a20c0643378a7a8098157ffb5745b79966208ee
roryclear <roryclear.rc@gmail.com>
2026-02-05 20:01:19 +0000

    more tiny

--- test.py
+++ test.py
@@ -1043,25 +1043,33 @@ class MSDeformAttn(nn.Module):
         self.value_proj = nn.Linear(d_model, d_model)
         self.output_proj = nn.Linear(d_model, d_model)
 
+        self.value_proj_tiny = tinynn.Linear(d_model, d_model)
+
         self._export = False
 
     def forward(self, query, reference_points, input_flatten, input_spatial_shapes,
                 input_level_start_index, input_padding_mask=None):
-        r"""
-        :param query                       (N, Length_{query}, C)
-        :param reference_points            (N, Length_{query}, n_levels, 2), range in [0, 1], top-left (0,0), bottom-right (1, 1), including padding area
-                                        or (N, Length_{query}, n_levels, 4), add additional (w, h) to form reference boxes
-        :param input_flatten               (N, \sum_{l=0}^{L-1} H_l \cdot W_l, C)
-        :param input_spatial_shapes        (n_levels, 2), [(H_0, W_0), (H_1, W_1), ..., (H_{L-1}, W_{L-1})]
-        :param input_level_start_index     (n_levels, ), [0, H_0*W_0, H_0*W_0+H_1*W_1, H_0*W_0+H_1*W_1+H_2*W_2, ..., H_0*W_0+H_1*W_1+...+H_{L-1}*W_{L-1}]
-        :param input_padding_mask          (N, \sum_{l=0}^{L-1} H_l \cdot W_l), True for padding elements, False for non-padding elements
+        
+        # todo move to weight load
+        self.value_proj_tiny.weight = to_tiny(self.value_proj.weight)
+        self.value_proj_tiny.bias = to_tiny(self.value_proj.bias)
 
-        :return output                     (N, Length_{query}, C)
-        """
+        query = to_tiny(query)
+        reference_points = to_tiny(reference_points)
+        input_flatten = to_tiny(input_flatten)
+        input_spatial_shapes = to_tiny(input_spatial_shapes)
+        input_padding_mask = to_tiny(input_padding_mask)
+
         N, Len_q, _ = query.shape
         N, Len_in, _ = input_flatten.shape
-        value = self.value_proj(input_flatten)
+
+        reference_points = to_torch(reference_points)
+        input_spatial_shapes = to_torch(input_spatial_shapes)
+
+        value = self.value_proj_tiny(input_flatten)
         value = value.masked_fill(input_padding_mask[..., None], float(0))
+        value = to_torch(value)
+        query = to_torch(query)
         sampling_offsets = self.sampling_offsets(query).view(N, Len_q, self.n_heads, self.n_levels, self.n_points, 2)
         attention_weights = self.attention_weights(query).view(N, Len_q, self.n_heads, self.n_levels * self.n_points)
 
