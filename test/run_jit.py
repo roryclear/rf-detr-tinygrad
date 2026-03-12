335817ad4766796240a7b3f5bcdca1ce811e2c96
roryclear <roryclear.rc@gmail.com>
2026-03-12 16:17:25 +0000

    include preprocess

--- test/run_jit.py
+++ test/run_jit.py
@@ -5,9 +5,12 @@ from rfdetr import RFDETR, COCO_CLASSES
 from tinygrad import Tensor, TinyJit
 import numpy as np
 import sys
+import time
 
 @TinyJit
-def jit_inf(im, model): return model(im)
+def jit_inf(im, model):
+  im = model.preprocess(im)
+  return model(im)
 if __name__ == "__main__":
   sizes = {'n': 'nano', 's': 'small', 'm': 'medium', 'l': 'large'}
   size = sys.argv[1]
@@ -20,5 +23,9 @@ if __name__ == "__main__":
   img = Tensor(img_np)
   processed_images = model.preprocess(img)
   output = model(processed_images).numpy()
-  for _ in range(3): jit_output = jit_inf(processed_images, model).numpy()
+  for _ in range(3): jit_output = jit_inf(img, model).numpy()
   np.testing.assert_allclose(jit_output, output, rtol=1e-7)
+
+  ts = time.time()
+  for _ in range(10): _ = jit_inf(img, model).numpy()
+  print(f"FPS {size}: {10 / (time.time() - ts):.2f}")
