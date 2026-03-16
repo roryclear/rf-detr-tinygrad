34fc22fa667d4ee8bd875b7a49f7b1d97beaa806
roryclear <roryclear.rc@gmail.com>
2026-03-16 19:36:24 +0000

    fix colors

--- test/test_outputs.py
+++ test/test_outputs.py
@@ -40,9 +40,9 @@ def sort_boxes(xyxy):
 if __name__ == "__main__":
   threshold = 0.5
   models = [[384, "nano"], [512, "small"], [576, "medium"], [704, "large"]]
-  image = cv2.imread("test/dog.jpg")
 
   for i in range(len(models)):
+    image = cv2.imread("test/dog.jpg")
     model = RFDETR(models[i][1])
     img_np = np.asarray(image)
     h, w = img_np.shape[:2]
@@ -57,11 +57,10 @@ if __name__ == "__main__":
     class_ids = class_ids[keep]
     boxes = boxes[keep]
     labels = [f"{COCO_CLASSES[class_id]}" for class_id in class_ids]
-    annotated_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     np.testing.assert_allclose(sort_boxes(boxes), sort_boxes(excepted_xyxys[i]), atol=0.5)
     for box, label, class_id in zip(boxes, labels, class_ids):
       x1, y1, x2, y2 = map(int, box)
-      color = ((int(class_id)*37)%255, (int(class_id)*17)%255, (int(class_id)*97)%255); cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2); cv2.rectangle(annotated_image, (x1, y1-18), (x1+len(label)*9, y1), color, -1); cv2.putText(annotated_image, label, (x1, y1 - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-    cv2.imwrite(f"annotated_image_{i}.jpg", annotated_image)
+      color = ((int(class_id)*37)%255, (int(class_id)*17)%255, (int(class_id)*97)%255); cv2.rectangle(image, (x1, y1), (x2, y2), color, 2); cv2.rectangle(image, (x1, y1-18), (x1+len(label)*9, y1), color, -1); cv2.putText(image, label, (x1, y1 - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.imwrite(f"annotated_image_{i}.jpg", image)
 
   print("PASSED")
