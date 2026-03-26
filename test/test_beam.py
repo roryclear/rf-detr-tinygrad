6ce5f324cec796f41856b9bd6f3514f2ba495db7
roryclear <roryclear.rc@gmail.com>
2026-03-26 23:55:01 +0000

    rand seed before each

--- test/test_beam.py
+++ test/test_beam.py
@@ -1,11 +1,13 @@
 from tinygrad import Tensor
 from rfdetr import RFDETR
 import numpy as np
-from tinygrad.helpers import fetch, Context
+from tinygrad.helpers import Context
 if __name__ == "__main__":
     model = RFDETR("nano")
     input = Tensor.randn(model.res, model.res, 3)
+    Tensor.manual_seed(1337)
     res = model(input).numpy()
+    Tensor.manual_seed(1337)
     with Context(BEAM=2): res_beam = model(input).numpy()
     np.testing.assert_allclose(res, res_beam, rtol=1e-4)
     print("passed")
